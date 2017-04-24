55cf0c7879eb1f79df2d5cfd07f80f8b556c79f8
Samoilov Yevhen <yevhen.samoilov@gmail.com>
2017-04-24 22:58:52 +0300

    fixed warnings

--- cardsLayout/src/main/java/ua/jenshensoft/cardslayout/views/ViewMeasureConfig.java
+++ cardsLayout/src/main/java/ua/jenshensoft/cardslayout/views/ViewMeasureConfig.java
@@ -7,11 +7,13 @@ public class ViewMeasureConfig {
     private final View view;
     private int lastWidth;
     private int lastHeight;
+    private int childCount;
 
     public ViewMeasureConfig(View view) {
         this.view = view;
         this.lastWidth = -1;
         this.lastHeight = -1;
+        this.childCount = -1;
     }
 
     public boolean needUpdateView() {
@@ -20,6 +22,6 @@ public class ViewMeasureConfig {
         boolean isEqual = measuredHeight == lastHeight && measuredWidth == lastWidth;
         lastWidth = measuredWidth;
         lastHeight = measuredHeight;
-        return isEqual;
+        return !isEqual;
     }
 }
