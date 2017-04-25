f12ffc3c8359e5bf9f68ee979b8effb6d7543a6f
Samoilov Yevhen <yevhen.samoilov@gmail.com>
2017-04-25 22:04:14 +0300

    fixed warnings

--- cardsLayout/src/main/java/ua/jenshensoft/cardslayout/views/ViewMeasureConfig.java
+++ cardsLayout/src/main/java/ua/jenshensoft/cardslayout/views/ViewMeasureConfig.java
@@ -1,6 +1,7 @@
 package ua.jenshensoft.cardslayout.views;
 
 import android.view.View;
+import android.view.ViewGroup;
 
 public class ViewMeasureConfig {
 
@@ -20,6 +21,12 @@ public class ViewMeasureConfig {
         int measuredWidth = view.getMeasuredWidth();
         int measuredHeight = view.getMeasuredHeight();
         boolean isEqual = measuredHeight == lastHeight && measuredWidth == lastWidth;
+        if (view instanceof ViewGroup) {
+            ViewGroup viewGroup = (ViewGroup) view;
+            int childCount = viewGroup.getChildCount();
+            isEqual = isEqual && this.childCount == childCount;
+            this.childCount = childCount;
+        }
         lastWidth = measuredWidth;
         lastHeight = measuredHeight;
         return !isEqual;
