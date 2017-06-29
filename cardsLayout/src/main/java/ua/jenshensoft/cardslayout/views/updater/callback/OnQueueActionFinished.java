70bb5db12fa705e6f312aa7b2497c2e277070ef8
Yevhen_Samoilov <Yevhen_Samoilov@epam.com>
2017-06-29 17:36:12 +0300

    added the handler timer

--- cardsLayout/src/main/java/ua/jenshensoft/cardslayout/views/updater/callback/OnQueueActionFinished.java
+++ cardsLayout/src/main/java/ua/jenshensoft/cardslayout/views/updater/callback/OnQueueActionFinished.java
@@ -1,12 +1,18 @@
 package ua.jenshensoft.cardslayout.views.updater.callback;
 
+import android.support.annotation.Nullable;
+
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+
+import ua.jenshensoft.cardslayout.util.HandlerTimer;
 
 public class OnQueueActionFinished {
 
     private boolean finished;
     private List<Action> actions;
+    @Nullable
+    private HandlerTimer handlerTimer;
 
     public void addAction(Action action) {
         if (finished) {
@@ -19,7 +25,18 @@ public class OnQueueActionFinished {
         }
     }
 
-    void finish() {
+    public void finishWithDelay(final long delay) {
+        if (delay > 0) {
+            cancelTimer();
+            handlerTimer = new HandlerTimer();
+            handlerTimer.schedule(this::finish, (int) delay);
+        } else {
+            finish();
+        }
+    }
+
+    public void finish() {
+        cancelTimer();
         finished = true;
         if (actions != null && !actions.isEmpty()) {
             for (Action action : actions) {
@@ -27,6 +44,15 @@ public class OnQueueActionFinished {
             }
             actions.clear();
             actions = null;
+        }
+    }
+
+    /* private methods */
+
+    private void cancelTimer() {
+        if (handlerTimer != null) {
+            handlerTimer.cancel();
+            handlerTimer = null;
         }
     }
 
