e56214a15f3e3327e42bc2db48ec18656ff84e26
Yevhen_Samoilov <Yevhen_Samoilov@epam.com>
2017-12-27 16:35:29 +0200

    extented the GameTableLayout

--- app/src/main/java/ua/jenshensoft/cardslayoutsample/views/CardsLayoutDefault.java
+++ app/src/main/java/ua/jenshensoft/cardslayoutsample/views/CardsLayoutDefault.java
@@ -2,8 +2,12 @@ package ua.jenshensoft.cardslayoutsample.views;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 
 import ua.jenshensoft.cardslayout.listeners.card.OnCardSwipedListener;
+import ua.jenshensoft.cardslayout.pattern.models.CardCoordinates;
+import ua.jenshensoft.cardslayout.util.FlagManager;
+import ua.jenshensoft.cardslayout.views.card.Card;
 import ua.jenshensoft.cardslayout.views.layout.CardsLayout;
 
 public class CardsLayoutDefault extends CardsLayout {
@@ -36,5 +40,13 @@ public class CardsLayoutDefault extends CardsLayout {
                 removeCardView(cardInfo.getCardPositionInLayout());
             }
         });
+    }
+
+    @Override
+    protected <CV extends View & Card> void setViewCoordinatesToStartPosition(Card card, CardCoordinates cardCoordinates) {
+        if (getGravityFlag().containsFlag(FlagManager.Gravity.RIGHT) || getGravityFlag().containsFlag(FlagManager.Gravity.LEFT)) {
+            cardCoordinates.setAngle(cardCoordinates.getAngle() + 90);
+        }
+        super.setViewCoordinatesToStartPosition(card, cardCoordinates);
     }
 }
